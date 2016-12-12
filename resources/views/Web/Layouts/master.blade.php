eca44e4b83ffdd4e25590778fe7f425065c444a2
Ahmed Said <info.matrixcode@gmail.com>
2016-12-13 01:06:18 +0200

    update demo

--- resources/views/Web/Layouts/master.blade.php
+++ resources/views/Web/Layouts/master.blade.php
@@ -98,59 +98,7 @@
     </div>
     <div id="real-body">
       <div id="leftside">
-        <div id="pox">
-          <div class="top"> </div>
-          <div class="body">
-            <div id="label-cover">
-              <div class="label_pdf"> </div>
-            </div>
-            <div style="padding-left:10px; padding-top:40px"> <a href="images/files/PRICELIST english..pdf" title="PRICELIST english..pdf" target="_blank"> <img src="images/download.jpg"/></a> </div>
-            <div id="label-cover">
-              <div class="label3"> </div>
-            </div>
-            <div style="width: 186px; margin: 0 auto; padding-top:40px;"> <a href="information.php?page=contact us"><img src="images/en.jpg" /> </a></div>
-            <div id="label-cover">
-              <div class="label"> </div>
-            </div>
-            <div id="follow"> <a href="http://www.facebook.com/sharmlandtours" target="_blank"><img src="images/facebook-icon.png" class="left" style="margin-right: 10px;" /></a> <a href="http://www.youtube.com/sharmland" target="_blank"><img src="images/youtube.png" class="left" /> </a></div>
-            <div style="clear:both;"></div>
-            <div id="label-cover">
-              <div class="label2"> </div>
-            </div>
-            <div style="width: 190px; height: 79px; margin: 0 auto;margin-top: 45px;"> <a href="http://www.holidaycheck.de/reisetipp-Reiseinformationen+Sharm+Land+Tours-zid_128801.html" target="_blank"> <img src="images/holidaycheck.png" alt="holidaycheck" /></a> </div>
-            <div style="width: 190px; margin: 0 auto; "> <a href="http://www.tripadvisor.com/Attraction_Review-g297555-d3266843-Reviews-Sharm_Land_Day_Tours-Sharm_El_Sheikh_South_Sinai_Red_Sea_and_Sinai.html" target="_blank"><img src="images/trip-advisor-icon.png" /></a></div>
-            <div id="label-cover">
-              <div class="label4"> </div>
-            </div>
-            <div style=" padding-top: 45px;width:196px; margin:0 auto;"> <a href="information.php?page=how-book"> <img src="images/ger.png" /></a> </div>
-            <div id="speciall_offers001"> <a href="show.php?id=44"> <img src="images/special_offers.jpg" /></a> </div>
-            <div id="newsletters">
-              <form action="index.php" method="get" style="padding-top: 191px;">
-                <input type="text" name="email" value="enter your mail" onclick="this.value=''" class="input" />
-                <input type="hidden" value="index" name="page" />
-                <input type="submit" name="news" value="" class="submit" />
-              </form>
-            </div>
-            <div  style="margin:10px 0px 0px 0px ;">
-              <div style="line-height: 10px; background: none;">
-                <div style="max-width: 160px; width: 160px; background: none; margin: 0 auto;">
-                  <object id="w4aaa9c4e6c122b4c4af9ecec7aeeb33a" data="http://static.hotelscombined.com.s3.amazonaws.com/swf/weather_widget.swf" height="272" style="margin: 0; padding: 0;" type="application/x-shockwave-flash" width="160">
-                    <param name="movie" value="http://static.hotelscombined.com.s3.amazonaws.com/swf/weather_widget.swf" />
-                    <param name="wmode" value="transparent">
-                    <param name="flashvars" value="station_id=HESH&amp;city_name=Sharm el-Sheikh&amp;language=en&amp;use_celsius=Yes&amp;skinName=Blue&amp;PID=163946&amp;ts=201401180634&amp;hideChangeSkin=No">
-                    <param name="allowNetworking" value="all">
-                    <param name="allowScriptAccess" value="always">
-                  </object>
-                  <a alt="Hotels Combined" href="http://widgets.hotelscombined.com/City/Weather/Sharm_el%20Sheikh.htm?use_celsius=Yes" style="margin: 0; padding: 0; text-decoration: none; background: none;" target="_blank" title="Hotels Combined">
-                  <div style="background: none; color: white; text-align: center; width: 160px; height: 17px; margin: 0px 0 0 0; padding: 5px 0 0 0; cursor: pointer; background: transparent url(http://static.hotelscombined.com.s3.amazonaws.com/Pages/WeatherWidget/Images/weather_blue_bottom.png) no-repeat; font-size: 12px; font-family: Arial,sans-serif; line-height: 12px; font-weight: bold;"> See 10-Day Forecast</div>
-                  </a>
-                  <div style="text-align: center; width: 160px;"> <a alt="Hotels Combined" href="http://www.hotelscombined.com" rel="nofollow" style="background: none; font-family: Arial,sans-serif; font-size: 9px; color: #777777;" title="Hotels Combined"> © HotelsCombined.com</a></div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="bottom"> </div>
-        </div>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis convallis felis. Pellentesque non urna mauris. Donec imperdiet lorem orci, quis efficitur sapien luctus quis. Aliquam dictum lacus risus, sed vulputate metus facilisis quis. Sed luctus rutrum dui, ut molestie velit mollis sed. Nam ut nisl tortor. Cras ut nisi lacinia, convallis lacus a, venenatis justo. Vivamus sit amet iaculis mi, nec suscipit nisl. Fusce pellentesque vestibulum maximus. Aliquam venenatis faucibus augue id auctor. Quisque tincidunt nisi dui, et condimentum diam vestibulum vel. Nulla imperdiet lectus eros, sit amet ultricies leo tristique ac. Mauris commodo id purus in imperdiet.
       </div>
         <!-- container -->
         @yield('content')
